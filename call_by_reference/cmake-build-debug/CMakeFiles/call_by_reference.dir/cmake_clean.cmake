b5f3288c45387c5d722d42d89f77e1af89d9523e
İbrahim Varola <varolaibrahim@gmail.com>
2021-05-26 17:22:44 +0300

    call_by_reference added

---
+++ call_by_reference/cmake-build-debug/CMakeFiles/call_by_reference.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/call_by_reference.dir/main.c.obj"
+  "call_by_reference.exe"
+  "call_by_reference.exe.manifest"
+  "call_by_reference.pdb"
+  "libcall_by_reference.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/call_by_reference.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
